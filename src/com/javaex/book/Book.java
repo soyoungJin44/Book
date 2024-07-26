3cac24922af5f8a0fab847b5d2a407004b31614d
soyoungJin44 <wlsthdud0412@naver.com>
2024-07-26 17:35:06 +0900

    .

---
+++ src/com/javaex/book/Book.java
@@ -0,0 +1,44 @@
+package com.javaex.book;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Book {
+
+	public static void main(String[] args) {
+
+		// 0. import java.sql.*;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			// 1. JDBC 드라이버 (Oracle) 로딩
+			// 2. Connection 얻어오기
+			// 3. SQL문 준비 / 바인딩 / 실행
+			// 4.결과처리
+		} catch (ClassNotFoundException e) {
+			System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+			// 5. 자원정리
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				System.out.println("error:" + e);
+			}
+		}
+
+	}
+
+}
--- src/com/javaex/book/Book2Vo.java
+++ src/com/javaex/book/Book2Vo.java
@@ -1,7 +1,97 @@
 package com.javaex.book;
 
 public class Book2Vo {
-	
-	
+		//필드
+		
+		private int bookId;
+		private String title;
+		private String pubs;
+		private String pubDate;
+		private int authorId;
+		private String name; // 작가이름
+		private String desc;
+		
+		
+		//생성자
+		public Book2Vo() {
+			super();
+		}
+		
+		public Book2Vo(int bookId, String title, String pubs, String pubDate, int authorId, String name, String desc) {
+			super();
+			this.bookId = bookId;
+			this.title = title;
+			this.pubs = pubs;
+			this.pubDate = pubDate;
+			this.authorId = authorId;
+			this.name = name;
+			this.desc = desc;
+		}
+		//메서드 gs
+
+		public int getBookId() {
+			return bookId;
+		}
+
+		public void setBookId(int bookId) {
+			this.bookId = bookId;
+		}
+
+		public String getTitle() {
+			return title;
+		}
+
+		public void setTitle(String title) {
+			this.title = title;
+		}
+
+		public String getPubs() {
+			return pubs;
+		}
+
+		public void setPubs(String pubs) {
+			this.pubs = pubs;
+		}
+
+		public String getPubDate() {
+			return pubDate;
+		}
+
+		public void setPubDate(String pubDate) {
+			this.pubDate = pubDate;
+		}
+
+		public int getAuthorId() {
+			return authorId;
+		}
+
+		public void setAuthorId(int authorId) {
+			this.authorId = authorId;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getDesc() {
+			return desc;
+		}
+
+		public void setDesc(String desc) {
+			this.desc = desc;
+		}
+
+		
+		//메서드 일반
+
+		@Override
+		public String toString() {
+			return "Book2Vo [bookId=" + bookId + ", title=" + title + ", pubs=" + pubs + ", pubDate=" + pubDate
+					+ ", authorId=" + authorId + ", name=" + name + ", desc=" + desc + "]";
+		}
 
 }
